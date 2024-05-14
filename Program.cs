7f89972166fb88fdcb56a0c3dfdb67f1a9616659
jab284 <jab284testing@yahoo.com>
2024-05-14 08:43:55 -0400

    text changes

--- Program.cs
+++ Program.cs
@@ -8,8 +8,8 @@
         {
             //format strings for registering name            
             System.Console.WriteLine(
-                $"Please enter your {inputName}.  It must be between {minLength} and {maxLength} alpha characters long.");
-            string input = Console.ReadLine(); //reads input from user
+            $"Please enter your {inputName}. It must be between {minLength} and {maxLength} alpha characters long."); //take out??
+            string? input = Console.ReadLine(); //reads input from user
             try //Exceptions --  for wrong length
             {
                 if (input.Length > maxLength || input.Length < minLength)
@@ -30,13 +30,13 @@
                 // return to break out of while loop
                 return input; //users input
             }
-            catch (IndexOutOfRangeException e)
+            catch (IndexOutOfRangeException)
             {
                 System.Console.WriteLine(
                     $"Your {inputName} must be between {minLength} and {maxLength} alpha characters long.");
                 System.Console.WriteLine("Please try again.");
             }
-            catch (FormatException e)
+            catch (FormatException)
             {
                 System.Console.WriteLine($"Your {inputName} must only be alpha characters");
                 System.Console.WriteLine("Please try again.");
@@ -49,16 +49,17 @@
     {
         while (true)
         {
-            System.Console.WriteLine("Main Menu:");
+            System.Console.WriteLine();
+            System.Console.WriteLine("TASK MENU:");
             System.Console.WriteLine("What would you like to do next?");
             System.Console.WriteLine();
-            System.Console.WriteLine("[1] Add a new todo");
-            System.Console.WriteLine("[2] View all todos");
+            System.Console.WriteLine("[1] Add a new ToDo task");
+            System.Console.WriteLine("[2] View all ToDo tasks");  //changed spelling - added apostrophe
             System.Console.WriteLine("[3] Exit");
             System.Console.WriteLine();
             System.Console.WriteLine("Please select an option:");
 
-            string input = Console.ReadLine(); //returns users input for selection
+            string? input = Console.ReadLine(); //returns users input for selection
             System.Console.WriteLine();
             try
             {
@@ -70,7 +71,7 @@
 
                 return selection;
             }
-            catch (Exception e)
+            catch (Exception)
             {
                 System.Console.WriteLine("Sorry!  Your selection is invalid.");
                 System.Console.WriteLine("Selection must be between 1 and 3.  Please try again.");
@@ -83,19 +84,19 @@
     static void DisplayAddTodoMenu(TodoService todoService, User activeUser)
     {
         System.Console.WriteLine("What todo would you like add?");
-        string input = Console.ReadLine();
+        string? input = Console.ReadLine();
         System.Console.WriteLine();
 
         Todo todo = todoService.AddTodo(input, activeUser);
 
-        System.Console.WriteLine($"Todo '{input}' was added to your list.");
+        System.Console.WriteLine($"ToDo Task '{input.ToUpper()}' was added to your list.");
         System.Console.WriteLine();
     }
 
     //Method Display All Todos
     static void DisplayAllTodosMenu(TodoService todoService, User activeUser)
     {
-        System.Console.WriteLine("YOUR LIST OF TODOS:");
+        System.Console.WriteLine("YOUR LIST OF TODO TASKS:");
         System.Console.WriteLine();
         List<Todo> todos = todoService.GetAllTodos(activeUser);
         foreach (Todo todo in todos)
@@ -110,23 +111,26 @@
     // Method to Display Register User Menu
     static User DisplayRegisterUserMenu(UserService userService)
     {
+        //System.Console.WriteLine("----------"); //maybe wrong spot
+        System.Console.WriteLine("REGISTER:");
         //Method to get First Name
-        System.Console.WriteLine("Before starting your list you must first register with us.");
+        System.Console.WriteLine("Before creating your list you must first register with us.");
+        System.Console.WriteLine("After registering with us, you will need to Login.");
+        System.Console.WriteLine();
         string
             firstName = GetAndValidateUserInput("First Name", 4,
                 20); // this is not working right for first name due to space - need to figure out
         System.Console.WriteLine();
-        System.Console.WriteLine(($"Welcome {firstName} "));
+        System.Console.WriteLine(($"Welcome {firstName}!"));
         System.Console.WriteLine();
         //Method to get User Name
         string userName = GetAndValidateUserInput("Username", 7, 10);
         System.Console.WriteLine();
         //Method to get Password
         string
-            password = GetAndValidateUserInput("Password", 5,
-                10); //not working as expected - cant do numbers right now - need to fix?
+            password = GetAndValidateUserInput("Password", 5, 10); //not working as expected - cant do numbers right now - need to fix?
         System.Console.WriteLine();
-        //Method to create RegisterUser with frist name, user name, and password - pulls from above
+        //Method to create RegisterUser with first name, user name, and password - pulls from above
         User user = userService.RegisterUser(firstName, userName, password);
         return user;
     }
@@ -135,15 +139,16 @@
     static User DisplayLoginMenu(UserService userService)
     {
         // Login - Menu name - UserName - Password - Success - return
-        System.Console.WriteLine("Welome to 'CREATE YOUR OWN TO DO LIST'.");
-        System.Console.WriteLine("Login Menu:");
+        System.Console.WriteLine("Welcome Back! ");
+        System.Console.WriteLine();
+        System.Console.WriteLine("LOGIN MENU:");
+        System.Console.WriteLine();
         string userName = GetAndValidateUserInput("Username", 7, 10);
         System.Console.WriteLine();
         string password = GetAndValidateUserInput("Password", 5, 10);
         System.Console.WriteLine();
-        System.Console.WriteLine();
         User user = userService.Login(userName, password);
-        System.Console.WriteLine();
+        //System.Console.WriteLine();
         return user;
     }
     
@@ -152,6 +157,7 @@
     {
         while (true)
         {
+            System.Console.WriteLine();
             System.Console.WriteLine("MAIN MENU");
             System.Console.WriteLine();
             System.Console.WriteLine("Please Register or Login:");
@@ -162,7 +168,9 @@
             System.Console.WriteLine();
             System.Console.WriteLine("Please select an option:");
 
-            string input = Console.ReadLine(); //returns users input for selection
+            string? input = Console.ReadLine(); //returns users input for selection
+            System.Console.WriteLine(); ///do i want to add line
+            System.Console.WriteLine("------------------------");
             System.Console.WriteLine();
             try
             {
@@ -174,13 +182,16 @@
 
                 return selection;
             }
-            catch (Exception e)
+            catch (Exception)
             {
                 System.Console.WriteLine("Sorry!  Your selection is invalid.");
                 System.Console.WriteLine("Selection must be between 1 and 3.  Please try again.");
                 System.Console.WriteLine();
             }
+            System.Console.WriteLine("-----------------------------------------------------");
+            System.Console.WriteLine();
         }
+       
     }
     
     
@@ -206,7 +217,7 @@
         System.Console.WriteLine("Here, you can easily maintain and track your list of need to do chores!");
         System.Console.WriteLine();
         System.Console.WriteLine("--------------------------");
-        System.Console.WriteLine();
+        //System.Console.WriteLine();
 
         User? user = null;  
 
@@ -234,7 +245,8 @@
                     }
                     break;
                 case 3:
-                    System.Console.WriteLine("See you later!");
+                    System.Console.WriteLine("Have a great day.  See you later!");
+                    System.Console.WriteLine();
                     exitRequested = true;
                     break;
                 default:
@@ -264,8 +276,9 @@
                     DisplayAllTodosMenu(todoService, user);
                     break;
                 case 3:
-                    System.Console.WriteLine("See you later!");
+                    System.Console.WriteLine("Have a great day.  See you later!");
                     exitRequested = true;
+                    System.Console.WriteLine(); 
                     break;
                 default:
                     System.Console.WriteLine("Invalid selection.  Please select, 1, 2, or 3.");
